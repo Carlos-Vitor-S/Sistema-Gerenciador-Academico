a5846148bbe1494e66b5ed4a98718e98e0007bab
carlos <carlosvitor179@protonmail.com>
2024-10-23 12:25:13 -0300

    Criação dos Serviços + Header Actions

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import css from "./Input.module.css";
 import TextField from "@mui/material/TextField";
-const Input = ({ label, register, name, isInputMargin = true }) => {
+const Input = ({
+  label,
+  register,
+  name,
+  isInputMargin = true,
+  type = "text",
+}) => {
   return (
     <TextField
       {...register(name)}
       id="outlined-basic"
       label={label}
-      type="text"
+      type={type}
       variant="outlined"
       placeholder={label}
       sx={isInputMargin ? { marginBottom: "1rem" } : undefined}
