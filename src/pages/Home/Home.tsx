8609e6479b39c52e26135736558f2f732085260c
carlos <carlosvitor179@protonmail.com>
2024-10-23 22:32:19 -0300

    Mudança Design e algumas melhorias

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import fabtechLogo from "../../assets/fabtech-logo.png";
+import css from "./Home.module.css";
 function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className={css.container}>
+      <div className={css.imageContainer}>
+        <img src={fabtechLogo} alt={fabtechLogo} />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
